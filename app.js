8bce0653daaccb5dc1a2e99398f6ab8a69f0c2a3
Shreyas Pande <shreyaspande2010@gmail.com>
2022-11-24 15:40:32 +0530

    more changes

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.post("/index",function(req,res){
 
 });
 
-app.listen(process.env.PORT||3000,function(){
-    console.log("Hosted on Port 3000!!!");
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
